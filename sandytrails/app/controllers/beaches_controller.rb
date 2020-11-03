ebe47f590bc3870a0fe8d0e451555dadc85b2c04
Erick-EEV <EEVERICKEEV@gmail.com>
2020-11-03 15:07:04 -0500

    Beaches Done

--- sandytrails/app/controllers/beaches_controller.rb
+++ sandytrails/app/controllers/beaches_controller.rb
@@ -1,22 +1,38 @@
 class BeachesController < ApplicationController
   def index
+    @beaches = Beach.all
   end
 
   def show
+    @beach = Beach.find(params[:id])
   end
 
   def create
+    beach = Beach.create(beach_params)
+    redirect_to beach_path(beach.id)
   end
 
   def new
+    @beach = Beach.new
   end
 
   def edit
+    @beach = Beach.find(params[:id])
   end
 
   def update
+    @beach = Beach.find(params[:id])
+    @beach.update(beach_params)
+    redirect_to beach_path(@beach.id)
   end
 
   def destroy
+    @beach_to_delete = Beach.find(params[:id])
+    @beach_to_delete.delete
+    redirect_to beaches_path
+  end
+
+  def beach_params
+    params.require(:beach).permit(:name, :location, :open)
   end
 end
--- sandytrails/config/routes.rb
+++ sandytrails/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- get '/', to: 'static#home'
+ get '/', to: 'static#home', as: 'home'
  get '/about', to: 'static#about'
 
+#  get '/beaches', to: 'beaches#index'
+#  get '/beaches/:id', to: 'beaches#show', as: 'beach'
 
-resources :visitor
-resources :beach
+resources :visitors
+resources :beaches
 
 end
