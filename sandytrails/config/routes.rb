00ed42f56cf9fc003357f35dd3ab1a348c0ab14d
chirptotheeast <alyssa.easterly@yahoo.com>
2020-11-03 13:05:20 -0600

    static controller

--- sandytrails/config/routes.rb
+++ sandytrails/config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'visitors/index'
-  get 'visitors/show'
-  get 'visitors/new'
-  get 'visitors/edit'
-  get 'beaches/index'
-  get 'beaches/show'
-  get 'beaches/create'
-  get 'beaches/new'
-  get 'beaches/edit'
-  get 'beaches/update'
-  get 'beaches/destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ get '/', to: 'static#home'
+ get '/about', to: 'static#about'
+
+
+resources :visitor
+resources :beach
+
 end
--- sandytrails/db/seeds.rb
+++ sandytrails/db/seeds.rb
@@ -6,3 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Visitor.destroy_all
+Beach.destroy_all
+Trip.destroy_all
+
+10.times do 
+            Visitor.create(name: Faker::Superhero.name,
+                           age: rand(15...60),
+                           city: Faker::Address.city,
+                           mermaid_descendant: [true, false].sample)
+end
+
+beaches = ['Miami Beach', 'Siesta Key', 'Myrtle Beach', 'Ocean City', 'Rehobeth Beach']
+
+
+beaches.each do |beach_name|
+        Beach.create(name: beach_name,
+                     location: Faker::Address.city,
+                     open: [true, false].sample)
+end
+
+5.times do  
+       Trip.create(visitor_id: Visitor.all.sample.id, beach_id: Beach.all.sample.id)
+end
